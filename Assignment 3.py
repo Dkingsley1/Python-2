6bc177d8423bc89653fdd27c8fcff218bdbb4172
Dkingsley1 <dan_kingsley@aol.com>
2026-01-21 10:28:57 -0500

    Add files via upload

---
+++ Assignment 3.py
@@ -0,0 +1,20 @@
+class QueueList:
+    def __init__(self):
+        self.items = []
+
+    def enqueue(self,item):
+        self.items.append(item)
+
+    def dequeue(self):
+        if len(self.items) < 1:
+            return None
+        return self.items.pop(0)
+
+    def size(self):
+        return len(self.items)
+
+# Quick Test
+q = QueueList()
+q.enqueue("First")
+q.enqueue("Second")
+print(q.dequeue()) # Output: First
