1d22ce619ace46775fe82d20ace5b8ee49a03d93
Dkingsley1 <dan_kingsley@aol.com>
2026-01-21 10:29:26 -0500

    Add files via upload

---
+++ Assignment 4.py
@@ -0,0 +1,16 @@
+from collections import deque
+
+class Queuedeque:
+    def __init__(self):
+        self.queue = deque()
+
+    def enqueue(self, item):
+        self.items.append(item):
+
+    def dequeue(self):
+        if len(self.items) < 1:
+            return None
+        return self.item.popleft() + Faster than pop (0)
+
+    def seize(self):
+        return len(self.items)
